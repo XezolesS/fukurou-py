85de52fcde001656f8b9fee31a1670e77a301586
Joe K <tndid7876@gmail.com>
2023-09-15 17:45:34 +0900

    Change directory of cog resources.

--- fukurou/cogs/minigames/cog.py
+++ fukurou/cogs/minigames/cog.py
@@ -1,8 +1,7 @@
+import random
 import discord
 from discord.ext import commands
-from discord.ext.bridge import BridgeContext
 from discord.ext.bridge import BridgeApplicationContext
-import random
 
 class MinigamesCog(commands.Cog):
     def __init__(self, bot):
@@ -11,6 +10,6 @@ class MinigamesCog(commands.Cog):
     @discord.slash_command()
     async def roll_the_dice(self, ctx: BridgeApplicationContext):
         dice_roll = random.randint(1, 6)
-        dice_image = discord.File(f"./fukurou/resources/minigames/dice/dice_{dice_roll}.png")
+        dice_image = discord.File(f"./fukurou/cogs/minigames/resources/dice_{dice_roll}.png")
 
         await ctx.respond(file = dice_image)
