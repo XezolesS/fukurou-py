8fb387a9e9132d63aa749e66c9eae3a3c3b3d7c2
Joe K <tndid7876@gmail.com>
2024-05-29 00:54:42 +0900

    Put descriptions to the configs module.

--- fukurou/configs/__init__.py
+++ fukurou/configs/__init__.py
@@ -1,3 +1,6 @@
+"""
+A module for managing system configs of fukurou contents.
+"""
 from .baseconfig import BaseConfig
 from .exceptions import NewConfigInterrupt
 from .service import ConfigService, Config, add_config, get_config
