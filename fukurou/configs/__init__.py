c8f79e2737225310addd558224e85fcf09fcd42e
Joe K <tndid7876@gmail.com>
2024-06-04 16:36:40 +0900

    Add `__all__`.

--- fukurou/configs/__init__.py
+++ fukurou/configs/__init__.py
@@ -4,3 +4,11 @@ A module for managing system configs of fukurou contents.
 from .baseconfig import BaseConfig, BaseSubConfig
 from .exceptions import NewConfigInterrupt
 from .service import ConfigService, Config, add_config, get_config
+
+__all__ = [
+    'BaseConfig',
+    'BaseSubConfig',
+    'Config',
+    'add_config',
+    'get_config'    
+]
